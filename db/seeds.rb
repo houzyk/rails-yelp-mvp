7945de952d7a3c4ad4cf348612a7a41e1d2fa065
houzairkoussa <houzairmk@icloud.com>
2021-10-27 11:54:14 +0400

    seeded restaurant db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning Database'
+Restaurant.destroy_all
+puts 'Creating 5 restaurants'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Faker::Restaurant.type
+  )
+  restaurant.save
+  puts "#{restaurant.name} has been created"
+end
+puts 'Created 5 restaurants'
